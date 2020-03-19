9c1728d2172095449f4ca6d0ac80c0b163e9a4aa
Mamednoor <mamed.gohabur@gmail.com>
2020-03-19 22:37:52 +0100

    i do it

--- script.js
+++ script.js
@@ -1,71 +1,80 @@
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml5 .line',
-    opacity: [0.5,1],
-    scaleX: [0, 1],
-    easing: "easeInOutExpo",
-    duration: 700
-  }).add({
-    targets: '.ml5 .line',
-    duration: 600,
-    easing: "easeOutExpo",
-    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
-  }).add({
-    targets: '.ml5 .ampersand',
-    opacity: [0,1],
-    scaleY: [0.5, 1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=600'
-  }).add({
-    targets: '.ml5 .letters-left',
-    opacity: [0,1],
-    translateX: ["0.5em", 0],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=300'
-  }).add({
-    targets: '.ml5 .letters-right',
-    opacity: [0,1],
-    translateX: ["-0.5em", 0],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=600'
-  }).add({
-    targets: '.ml5',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+anime.timeline({
+        loop: true
+    })
+    .add({
+        targets: '.ml5 .line',
+        opacity: [0.5, 1],
+        scaleX: [0, 1],
+        easing: "easeInOutExpo",
+        duration: 700
+    })
+
+    .add({
+        targets: '.ml5 .line',
+        duration: 600,
+        easing: "easeOutExpo",
+        translateY: (el, i) => (-0.625 + 0.625 * 2 * i) + "em"
+    })
+
+    .add({
+        targets: '.ml5 .ampersand',
+        opacity: [0, 1],
+        scaleY: [0.5, 1],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=600'
+    })
+
+    .add({
+        targets: '.ml5 .letters-left',
+        opacity: [0, 1],
+        translateX: ["0.5em", 0],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=300'
+    })
+
+    .add({
+        targets: '.ml5 .letters-right',
+        opacity: [0, 1],
+        translateX: ["-0.5em", 0],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=600'
+    }).add({
+        targets: '.ml5',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
 
 
 
-  let test = document.getElementById("bodytest");
-  
-  // Ce gestionnaire ne sera exécuté qu'une fois
-  // lorsque le curseur se déplace sur la liste
-  test.addEventListener("mouseenter", function( event ) {   
+let test = document.getElementById("bodytest");
+
+// Ce gestionnaire ne sera exécuté qu'une fois
+// lorsque le curseur se déplace sur la liste
+test.addEventListener("mouseenter", function (event) {
     // on met l'accent sur la cible de mouseenter
     event.target.style.color = "purple";
-  
+
     // on réinitialise la couleur après quelques instants
-    setTimeout(function() {
-      event.target.style.color = "";
+    setTimeout(function () {
+        event.target.style.color = "";
     }, 500);
-  }, false);
-  
-  // Ce gestionnaire sera exécuté à chaque fois que le curseur
-  // se déplacera sur un autre élément de la liste
-  test.addEventListener("mouseover", function( event ) {   
-    // on met l'accent sur la cible de mouseover
-    event.target.style.color = "orange";
-  
-    // on réinitialise la couleur après quelques instants
-    setTimeout(function() {
-      event.target.style.color = "";
-    }, 
-    500);
-  },
-   false);
-  
+}, false);
+
+// Ce gestionnaire sera exécuté à chaque fois que le curseur
+// se déplacera sur un autre élément de la liste
+test.addEventListener("mouseover", function (event) {
+        // on met l'accent sur la cible de mouseover
+        event.target.style.color = "orange";
+
+        // on réinitialise la couleur après quelques instants
+        setTimeout(function () {
+                event.target.style.color = "";
+            },
+            500);
+    },
+    false);
